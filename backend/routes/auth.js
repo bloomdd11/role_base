a712bd4dc452510d51208f988559c938dfb4bdfa
kettlothsuhoo <kh685132@gmail.com>
2022-12-09 23:25:33 +0630

    add auth methods

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,53 @@
 const express = require('express')
 const router = express.Router()
+const bcrpyt = require('bcrypt')
 const { body, validationResult } = require('express-validator')
 const CustomAPIError = require('../middleware/custom-api')
 const User = require('../model/User')
 
-router.post('/login', async (req, res) => {
-  return res.json({ return: true, msg: 'login' })
+router.post('/signup', [
+  body('username').not().isEmpty(),
+  body('password').not().isEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    throw new CustomAPIError('invalid value', 400)
+  }
+
+  const { username, password } = req.body
+  const temp_user = { username: username, password: await bcrpyt.hash(password, 10) }
+
+  const user = new User(temp_user)
+  try {
+    await user.save()
+  } catch (error) {
+    throw new CustomAPIError(error)
+  }
+
+  return res.json({ return: true, msg: user })
 })
-router.post('/signup', async (req, res) => {
-  return res.json({ return: true, msg: 'signup' })
+
+router.post('/login', [
+  body('username').not().isEmpty(),
+  body('password').not().isEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    throw new CustomAPIError('invalid value', 400)
+  }
+
+  const { username, password } = req.body
+  const [user] = await User.find({ username })
+  if (user.length === 0) {
+    throw new CustomAPIError('No user is found with that name', 404)
+  }
+
+  const checkPWD = await bcrpyt.compare(password, user.password)
+  if (!checkPWD) {
+    throw new CustomAPIError('Incorrect password', 400)
+  }
+
+  return res.json({ return: true, msg: user })
 })
 
 module.exports = router
